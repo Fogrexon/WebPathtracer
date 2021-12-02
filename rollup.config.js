b11e057ebe655198689ed103d11e12364f5cb0b0
FogRexOn <50396924+Fogrexon@users.noreply.github.com>
2021-12-03 00:35:09 +0900

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,88 @@
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+const extensions = ['.ts', '.js'];
+
+export default [
+
+  {
+    input: 'src/index.ts',
+
+    preserveModules: true,
+
+    output: {
+      dir: 'build/commonjs',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+
+    plugins: [
+      nodeResolve({browser: true}),
+      postcss({
+        extract: true,
+      }),
+      babel({
+        extensions,
+      }),
+      typescript({
+        declaration: true,
+        rootDir: 'src',
+        declarationDir: 'build/commonjs/src',
+      }),
+    ],
+  },
+
+  {
+    input: 'src/index.ts',
+    preserveModules: true,
+    output: {
+      dir: 'build/es',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
+
+    plugins: [
+      nodeResolve({browser: true}),
+      postcss({
+        extract: true,
+      }),
+      babel({
+        extensions,
+      }),
+      typescript({
+        declaration: true,
+        rootDir: 'src',
+        declarationDir: 'build/es/src',
+      }),
+    ],
+  },
+
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'build/umd/graphim.js',
+      // dir: 'build/umd',
+      format: 'umd',
+      name: 'Graphim',
+      sourcemap: true,
+    },
+    plugins: [
+      nodeResolve({browser: true}),
+      postcss({
+        extract: true,
+      }),
+      babel({
+        extensions,
+      }),
+      typescript({
+        declaration: true,
+        rootDir: 'src',
+        declarationDir: 'build/umd',
+      }),
+    ],
+  },
+];
