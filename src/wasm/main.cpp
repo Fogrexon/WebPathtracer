d9914ea23c92337760d6dce59a90e863ce4ef1fc
kegra <nukegarara@yahoo.co.jp>
2021-12-11 21:21:55 +0900

    少しずつ描画(wip)

--- src/wasm/main.cpp
+++ src/wasm/main.cpp
@@ -18,8 +18,19 @@ int main(int argc, char **argv) {
 extern "C" {
 #endif
 
-ModelBVH bvh;
-camera cam;
+struct renderingStream {
+  bool working = false;
+  struct {
+    int width, height;
+    ModelBVH bvh;
+    camera cam;
+  } settings;
+  struct {
+    int j;
+    std::vector<std::vector<Raytracer::Vec3>> rawPixels;
+  } progress;
+};
+renderingStream stream;
 
 int EMSCRIPTEN_KEEPALIVE createBounding(float* position, int posCount, int* indicies, int indexCount, float* normal, int normCount, float* texCoord, int texCoordCount) {
   std::vector<vert> vertex;
@@ -36,7 +47,7 @@ int EMSCRIPTEN_KEEPALIVE createBounding(float* position, int posCount, int* indi
     polygon.push_back(p);
   }
 
-  bvh.construct(vertex, polygon);
+  stream.settings.bvh.construct(vertex, polygon);
 
   return 0;
 }
@@ -48,71 +59,95 @@ int EMSCRIPTEN_KEEPALIVE setCamera(float* camData) {
   // printf("camRight %f %f %f\n", camData[9], camData[10], camData[11]);
   // printf("dist %f\n", camData[12]);
 
-  cam.pos = Raytracer::Vec3{camData[0], camData[1], camData[2]};
-  cam.forward = Raytracer::Vec3{camData[3], camData[4], camData[5]};
-  cam.camUp = Raytracer::Vec3{camData[6], camData[7], camData[8]};
-  cam.camRight = Raytracer::Vec3{camData[9], camData[10], camData[11]};
-  cam.dist = camData[12];
+  stream.settings.cam.pos = Raytracer::Vec3{camData[0], camData[1], camData[2]};
+  stream.settings.cam.forward = Raytracer::Vec3{camData[3], camData[4], camData[5]};
+  stream.settings.cam.camUp = Raytracer::Vec3{camData[6], camData[7], camData[8]};
+  stream.settings.cam.camRight = Raytracer::Vec3{camData[9], camData[10], camData[11]};
+  stream.settings.cam.dist = camData[12];
+  return 0;
+}
 
+int EMSCRIPTEN_KEEPALIVE readStream(int* a){
+  if(!stream.working) {
+    return -1;
+  }
+
+  int width = stream.settings.width, height = stream.settings.height;
+  const int lineperupdate = 1;
+
+  if(stream.progress.j < stream.settings.height){
+      int j;
+      for(j = stream.progress.j; j < height && j < stream.progress.j + lineperupdate; j++){
+          for(int i = 0; i < width; i++){
+              const int spp = 3;
+              Raytracer::Vec3 resultRgb{};
+              for(int s = 0; s < spp; s++) {
+                  // heightを1とした正規化
+                  Raytracer::Ray ray = stream.settings.cam.getRay(
+                    (double(i) + Raytracer::rnd() - width / 2) / height,
+                    -(double(j) + Raytracer::rnd() - height / 2) / height);
+                  resultRgb += Raytracer::raytrace(ray, stream.settings.bvh).rgb;
+              }
+              resultRgb *= (double(1.0) / spp);
+
+              stream.progress.rawPixels[j][i] = resultRgb;
+          }
+      }
+      stream.progress.j = j;
+      return 1;
+  }
+  
+  // 3x3 gaussian
+  // constexpr int kernelW = 3, kernelH = 3;
+  // double filterKernel[kernelW][kernelH] = {
+  //   {1.0/16, 2.0/16, 1.0/16},
+  //   {2.0/16, 4.0/16, 2.0/16},
+  //   {1.0/16, 2.0/16, 1.0/16}
+  // };
+  constexpr int kernelW = 1, kernelH = 1;
+  double filterKernel[kernelW][kernelH] = {
+    {1.0}
+  };
+  const double gamma = 1/2.2;
+
+  for(int j = 0; j < height; j++){
+    for(int i = 0; i < width; i++){
+      Raytracer::Vec3 resultRgb{};
+
+      for(int dx = 0; dx < kernelW; dx++){
+        for(int dy = 0; dy < kernelH; dy++){
+          int sx = std::clamp(i + dx - kernelW / 2, 0, width - 1);
+          int sy = std::clamp(j + dy - kernelH / 2, 0, height - 1);
+          resultRgb += filterKernel[dx][dy] * stream.progress.rawPixels[sy][sx];
+        }
+      }
+      resultRgb.x = pow(resultRgb.x, gamma);
+      resultRgb.y = pow(resultRgb.y, gamma);
+      resultRgb.z = pow(resultRgb.z, gamma);
+
+      int index = j * width + i;
+      a[index * 4 + 0] = resultRgb.x * 255;
+      a[index * 4 + 1] = resultRgb.y * 255;
+      a[index * 4 + 2] = resultRgb.z * 255;
+      a[index * 4 + 3] = 255;
+    }
+  }
+  
+  stream.working = false;
   return 0;
 }
 
 int EMSCRIPTEN_KEEPALIVE pathTracer(int* a, int width, int height){
-    std::vector<std::vector<Raytracer::Vec3>> rawPixels(height, std::vector<Raytracer::Vec3>(width));
-
-    int index = 0;
-
-    for(int j = 0; j < height; j++){
-        for(int i = 0; i < width; i++){
-            const int spp = 3;
-            Raytracer::Vec3 resultRgb{};
-            for(int s = 0; s < spp; s++) {
-                // heightを1とした正規化
-                Raytracer::Ray ray = cam.getRay(
-                  (double(i) + Raytracer::rnd() - width / 2) / height,
-                  -(double(j) + Raytracer::rnd() - height / 2) / height);
-                resultRgb += Raytracer::raytrace(ray, bvh).rgb;
-            }
-            resultRgb *= (double(1.0) / spp);
-
-            rawPixels[j][i] = resultRgb;
-        }
+    if(stream.working){
+      return -1;
     }
-    
-    // 3x3 gaussian
-    // constexpr int kernelW = 3, kernelH = 3;
-    // double filterKernel[kernelW][kernelH] = {
-    //   {1.0/16, 2.0/16, 1.0/16},
-    //   {2.0/16, 4.0/16, 2.0/16},
-    //   {1.0/16, 2.0/16, 1.0/16}
-    // };
-    constexpr int kernelW = 1, kernelH = 1;
-    double filterKernel[kernelW][kernelH] = {
-      {1.0}
-    };
-    const double gamma = 1/2.2;
-    for(int j = 0; j < height; j++){
-      for(int i = 0; i < width; i++){
-        Raytracer::Vec3 resultRgb{};
+    stream.working = true;
 
-        for(int dx = 0; dx < kernelW; dx++){
-          for(int dy = 0; dy < kernelH; dy++){
-            int sx = std::clamp(i + dx - kernelW / 2, 0, width - 1);
-            int sy = std::clamp(j + dy - kernelH / 2, 0, height - 1);
-            resultRgb += filterKernel[dx][dy] * rawPixels[sy][sx];
-          }
-        }
-        resultRgb.x = pow(resultRgb.x, gamma);
-        resultRgb.y = pow(resultRgb.y, gamma);
-        resultRgb.z = pow(resultRgb.z, gamma);
-
-        a[index * 4 + 0] = resultRgb.x * 255;
-        a[index * 4 + 1] = resultRgb.y * 255;
-        a[index * 4 + 2] = resultRgb.z * 255;
-        a[index * 4 + 3] = 255;
-        index++;
-      }
-    }
+    stream.settings.width = width;
+    stream.settings.height = height;
+    stream.progress.rawPixels.clear();
+    stream.progress.rawPixels.assign(height, std::vector<Raytracer::Vec3>(width));
+    stream.progress.j = 0;
 
     return 0;
 }
